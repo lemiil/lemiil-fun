26832b0ac6bb611e215e6568380da09880d7bd1b
lemiil <gavriillemeshko10@gmail.com>
2025-04-16 17:13:40 +0300

    typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@ import layout from './Layout.vue';
         <p> I'm Lemiil, a web developer. </p>
         <p>I've been doing this for quite a while and have a solid understanding of programming and the web in
             particular. </p>
-        <p>I value elegance and simplicity in technology—after all, as Terry Davis once said, "An idiot admires
+        <p>I value elegance and simplicity in technology — after all, as Terry Davis once said, "An idiot admires
             complexity,
             a genius admires simplicity." </p>
         <p> That's exactly why the only modern tech used on this site is Vue. Everything else was handcrafted by me.
