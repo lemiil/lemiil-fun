089f57ddf997e0761b17835a021b26c5f35b025f
lemiil <gavriillemeshko10@gmail.com>
2025-04-23 20:32:37 +0300

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,13 @@ import layout from './Layout.vue';
 
 <template>
     <layout>
-        <h1>About me: nothing</h1>
+        <section class="flex justify-center pt-20">
+            <a href="https://git.io/streak-stats" target="_blank" rel="noopener noreferrer">
+                <img class="rounded-md hover:scale-105 transition-transform duration-300 ease-in-out"
+                    src="https://github-readme-streak-stats.herokuapp.com?user=lemiil&theme=dark&border_radius=4&mode=weekly"
+                    alt="GitHub Streak" />
+            </a>
+        </section>
     </layout>
 
 </template>
