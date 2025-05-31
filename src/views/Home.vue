395da0f88552d07a126ff6b01c8414980b2851b6
lemiil <gavriillemeshko10@gmail.com>
2025-05-31 17:46:34 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,25 +4,7 @@ import layout from './Layout.vue';
 
 <template>
     <layout>
-        <section class="flex justify-start">
-            <figure class="max-w-screen-md mx-auto text-center">
-                <svg class="w-6 h-6 mx-auto mt-6 text-gray-400 dark:text-gray-600" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
-                    <path
-                        d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
-                </svg>
-                <blockquote
-                    class="p-6 my-6 text-3xl italic font-medium text-gray-900 dark:text-white border-s-4 border-gray-300 bg-coolblack">
-                    “An idiot admires complexity, a genius admires simplicity.”
-                </blockquote>
-                <figcaption class="flex items-center justify-center mt-6 mb-12 space-x-4 rtl:space-x-reverse">
-                    <div class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700">
-                        <cite class="pe-4 font-medium text-gray-900 dark:text-white">Terry Davis</cite>
-                    </div>
-                </figcaption>
-            </figure>
-        </section>
-
+ 
         <section class="flex justify-start">
             <div class="space-y-6 text-left max-w-3xl pr-10">
                 <h1 class="text-3xl font-bold">Hey there!</h1>
