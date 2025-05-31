1c026a4fbf9b99b3e79318126eb8fea956f7e027
lemiil <gavriillemeshko10@gmail.com>
2025-05-31 17:52:24 +0300

    deleted showing off

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,6 @@ import layout from './Layout.vue';
             <div class="space-y-6 text-left max-w-3xl pr-10">
                 <h1 class="text-3xl font-bold">Hey there!</h1>
                 <p class="text-xl">I'm <span class="font-semibold">Lemiil</span>, a web developer.</p>
-                <p class="text-lg">
-                    I've been doing this for quite a while and have a solid understanding of programming and the web in
-                    particular.
-                </p>
                                <p class="text-lg">The
                     <router-link class="font-semibold underline transition" :to="{ name: 'About' }">about
                         me</router-link>
