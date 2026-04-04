5f8020f0c5eab6da4978e325b3f8a3eedaab5f64
lemiil <gavriillemeshko10@gmail.com>
2026-04-04 16:13:08 +0300

    upd

--- src/router.js
+++ src/router.js
@@ -2,12 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import FlipCoin from './views/FlipCoin.vue'
-import Dev from './views/Dev.vue'
 
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/dev', name: 'Dev', component: Dev },
     { path: '/dev/about', name: 'About', component: About },
     { path: '/coin', name: 'FlipCoin', component: FlipCoin }
 ]
